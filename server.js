d5cd7bb9a5d8e198bff3937432f9de368675f9fe
Matt Wilkinson <mattwilki17@gmail.com>
2020-09-08 13:14:04 -0400

    exercise

--- server.js
+++ server.js
@@ -165,6 +165,152 @@ app.get('/api/shorturl/:hash', (req, res) => {
   })
 })
 
+const userSchema = new Schema({
+  username: {type: String, required: true},
+  exercises: [{description: String, duration: Number, date: Date}]
+});
+
+const User = mongoose.model('User', userSchema);
+
+const createAndSaveUser = (username, done) => {
+  const user = new User({username: username});
+
+
+  user.save((err, user) => {
+    if (err) return console.error(err);
+    done(null, user);
+  })
+}
+const doesUserExist = (username, done) => {
+  User.exists({username: username}, (err, exists) => {
+    if (err) return console.error(err);
+    done(null, exists);
+  })
+};
+
+const getAllUsers = (done) => {
+  User.find({})
+  .select('-exercises')
+  .exec((err, arr) => {
+    if (err) return console.error(err);
+    done(null, arr);
+  });
+}
+
+const addExerciseToUser = (userId, description, duration, date, done) => {
+  User.findOne({_id: userId}, (err, user) => {
+    const exercise = {'description': description, 'duration': duration, 'date': date}
+    console.log(user);
+    user.exercises.push(exercise);
+    user.save((err, user) => {
+      if (err) return console.error(err);
+      done(null, user);
+    })
+  })
+}
+
+app.post('/api/exercise/new-user', (req, res) => {
+  const inputUsername = req.body.username;
+
+  doesUserExist(inputUsername, (err, exists) => {
+    if (err) return console.error(err);
+
+    if (exists) {
+      res.send('Username already taken');
+      return;
+    }
+    
+    createAndSaveUser(inputUsername, (err, user) => {
+      if (err) return console.error(err);
+      res.send({username: user.username, _id: user._id});
+    });
+  });
+  
+});
+
+app.get('/api/exercise/users', (req, res) => {
+  getAllUsers((err, arr) => {
+    res.send(arr);
+  })
+});
+
+app.post('/api/exercise/add', (req, res) => {
+  const userId = req.body.userId;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  let date = new Date(req.body.date);
+
+  if(isNaN(date)) {
+    date = new Date();
+  }
+
+  addExerciseToUser(userId, description, duration, date, (err, user) => {
+    res.send({
+      '_id': user._id,
+      username: user.username,
+      date: date.toString,
+      duration: duration,
+      description: description
+    });
+  });
+
+});
+
+
+
+app.get('/api/exercise/log', (req, res) => {
+  const userId = req.query.userId;
+  const fromDate = new Date(req.query.from);
+  const toDate = new Date(req.query.to);
+  const limit = req.query.limit;
+
+  User.findOne({'_id': userId}, (err, user) => {
+    let filteredExercises;
+
+    if(!isNaN(fromDate) && !isNaN(toDate)) {
+      filteredExercises = user.exercises.filter(exercise => {
+        if(exercise.date >= fromDate || exercise.date <= toDate) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    } else if(!isNaN(fromDate)) {
+      filteredExercises = user.exercises.filter(exercise => {
+        if(exercise.date >= fromDate) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    } else if(!isNaN(toDate)) {
+      filteredExercises = user.exercises.filter(exercise => {
+        if(exercise.date <= toDate) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    } else {
+      filteredExercises = user.exercises;
+    }
+
+    if(!isNaN(limit)) {
+      filteredExercises = filteredExercises.slice(0, limit);
+    }
+
+    user.exercises = filteredExercises;
+    const count = user.exercises.length;
+    // const output = {_id: user._id, username: user.username, exercises: user.exercises, }
+    const output = {...user._doc, count: count};
+    console.log(output);
+
+    res.send(output);
+  })
+
+});
+
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 5000, function () {
